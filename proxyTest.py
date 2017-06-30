0b331d640a16b356f5dd79a591626a3fcaed7f0a
root <dtellez@cygitsolutions.com>
2017-06-30 09:58:07 +0200

    Prueb ProxyHandler

--- proxyTest.py
+++ proxyTest.py
@@ -29,14 +29,17 @@ proxy_username = config_section_map("ServerConfig")['proxy_username']
 proxy_password = config_section_map("ServerConfig")['proxy_password']
 
 proxy_connection_string = 'http://' + proxy_username + ":" + proxy_password + "@" + proxy_host + ":" + proxy_port
-	
+
+os.environ['http_proxy'] = '127.0.0.1'
+os.environ['https_proxy'] = '127.0.0.1'	
+
 if proxy_host:
 	print "Proxy mode"
-	proxy = urllib2.ProxyHandler({'https': proxy_connection_string})
+	proxy = urllib2.ProxyHandler()
 	auth = urllib2.HTTPBasicAuthHandler()
 	opener = urllib2.build_opener(proxy, auth, urllib2.HTTPHandler)
 	urllib2.install_opener(opener)
   
-urllib2.urlopen("http://google.es")
+urllib2.urlopen("https://google.es")
 
 print "Test completed"
